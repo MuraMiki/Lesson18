59b4e100bd417d118d7bc032e0225d80b794b091
MuraMiki <imobatake29@gmail.com>
2025-06-03 20:58:40 +0900

    Chapter 9.5

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,8 +1,10 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set; // 追加
+
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional; // 追加
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -20,6 +22,7 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
     /** Userを1件検索して返す */
     public User getUser(Integer id) {
         return userRepository.findById(id).get();
@@ -31,5 +34,13 @@ public class UserService {
         return userRepository.save(user);
     }
 
-
+    // ----- 追加:ここから -----
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
+    // ----- 追加:ここまで -----
 }
